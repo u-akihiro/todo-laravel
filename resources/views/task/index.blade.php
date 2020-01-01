73743de68499e0055f2c921ebe6cb8945d7130a5
Akihiro Uesaka <akihiro.uesaka@gmail.com>
2020-01-01 10:26:58 +0900

    タスク一覧を作成

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,9 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <ul>
-    @foreach ($tasks as $task)
-        <li>{{$task->title}}</li>
-    @endforeach
-    </ul>
+<div class="container">
+    <div class="row">
+        @foreach ($tasks as $task)
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{$task->title}}
+                    </h5>
+                    <p>
+                        {{$task->detail}}
+                    </p>
+                </div>
+                <div class="card-body">
+                    <div class="btn-group">
+                        <form action="/task/{{$task->id}}" method="POST">
+                            @csrf
+                            @method('post')
+                            <button class="btn btn-primary mr-2" type="submit">完了</button>
+                        </form>
+                        <form action="/task/{{$task->id}}" method="POST">
+                            @csrf
+                            @method('post')
+                            <button class="btn btn-danger" type="submit">削除</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    登録日：{{$task->created_at}}
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
